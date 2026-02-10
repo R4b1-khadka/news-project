49912ca39918735fe087d83f3f2f53fc36443178
R4b1-khadka <rabikhadka888@gmail.com>
2026-02-10 19:28:40 +0545

    logic to delete image from the local folder

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -5,6 +5,14 @@ $post_id= $_GET['id'];
 
 $cat_id= $_GET['catid'];
 
+
+$sql1="SELECT * FROM post WHERE post_id = {$post_id};";
+$result= mysqli_query($conn,$sql1) or die('query failed: select');
+$row= mysqli_fetch_assoc($result);
+
+unlink('upload/'.$row['post_img']);
+
+
 $sql="DELETE FROM post WHERE post_id={$post_id};";
 $sql .="UPDATE category SET post = post-1 WHERE category_id={$cat_id}";
 
@@ -12,4 +20,4 @@ if(mysqli_multi_query($conn,$sql)){
     header ("location: {$hostname}/admin/post.php");
 }else{
     echo "query Failed";
-}
+} 
