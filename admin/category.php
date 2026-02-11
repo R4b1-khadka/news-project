7a614cdf0a0c2165ca54be663354480358606b69
R4b1-khadka <rabikhadka888@gmail.com>
2026-02-11 13:34:10 +0545

    update: make category page dynamic for admin

--- admin/category.php
+++ admin/category.php
@@ -10,6 +10,14 @@
             </div>
             <div class="col-md-12">
                 <table class="content-table">
+                    <?php
+                    include "config.php";
+                    $sql= "SELECT * FROM category";
+                    $result= mysqli_query($conn,$sql) or die("query failed");
+                  
+                
+                    ?>
+
                     <thead>
                         <th>S.No.</th>
                         <th>Category Name</th>
@@ -18,49 +26,21 @@
                         <th>Delete</th>
                     </thead>
                     <tbody>
+                        <?php if(mysqli_num_rows($result)>0){
+                        while($row=mysqli_fetch_assoc($result)){ ?>
                         <tr>
-                            <td class='id'>1</td>
-                            <td>Html</td>
-                            <td>5</td>
+                            <td class='id'><?php echo $row['category_id']; ?></td>
+                            <td><?php echo $row['category_name']; ?></td>
+                            <td><?php echo $row['post']; ?></td>
                             <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
                             <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
                         </tr>
-                        <tr>
-                            <td class='id'>2</td>
-                            <td>Css</td>
-                            <td>15</td>
-                            <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
-                            <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
-                        </tr>
-                        <tr>
-                            <td class='id'>3</td>
-                            <td>Java</td>
-                            <td>8</td>
-                            <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
-                            <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
-                        </tr>
-                        <tr>
-                            <td class='id'>4</td>
-                            <td>Php</td>
-                            <td>11</td>
-                            <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
-                            <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
-                        </tr>
-                        <tr>
-                            <td class='id'>5</td>
-                            <td>Python</td>
-                            <td>13</td>
-                            <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
-                            <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
-                        </tr>
-                        <tr>
-                            <td class='id'>6</td>
-                            <td>Scss</td>
-                            <td>3</td>
-                            <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
-                            <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
-                        </tr>
+                        
                     </tbody>
+                    <?php 
+                        }
+                    }
+                    ?>
                 </table>
                 <ul class='pagination admin-pagination'>
                     <li class="active"><a>1</a></li>
